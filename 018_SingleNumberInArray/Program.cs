8168bf1aab5e1c2411cc8f9f3e36d9b011086807
Yogananda Rao Locherla <locherla@live.com>
2022-10-13 22:17:09 +0530

    Single occurance element in an array

---
+++ 018_SingleNumberInArray/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+ *  Problem Description
+        Given an array of integers A, every element appears twice except for one. Find that integer that occurs once.
+
+        NOTE: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+  
+    Output Format
+        Return a single integer denoting the single element.
+
+
+    Example Input
+        Input:
+            A = [1, 2, 2, 3, 1]
+        Output:
+            3
+
+        Input:
+            A = [1, 2, 2]
+        Output:
+            1
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace _018_SingleNumberInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputA = new List<int> { 1, 2, 3, 4, 5, 1, 2, 3, 4 };
+            Console.WriteLine(SingleElementInArray(inputA) == 5 ? "Passed" : "Failed");
+        }
+
+        public static int SingleElementInArray(List<int> A)
+        {
+            var ans = 0;
+            foreach (var item in A)
+            {
+                ans ^= item;            // A ^ A = 0 ==> XOR of all array elements results the element of single occurance.
+            }
+
+            return ans;
+        }
+    }
+}
